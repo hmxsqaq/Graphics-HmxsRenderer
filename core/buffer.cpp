0c9e4d4434db14767cf44c64e357584cb606b9ad
hmxs <hmxsqaq@163.com>
2024-07-19 02:07:25 +0800

    feat(buffer): implement DepthBuffer

--- core/buffer.cpp
+++ core/buffer.cpp
@@ -21,18 +21,51 @@ std::uint8_t ColorBuffer::operator[](const size_t index) const {
     return buffer_[index];
 }
 
-void ColorBuffer::Clear(const uint8_t value) const {
-    std::fill_n(buffer_.get(), width_ * height_ * bpp_, value);
-}
-
 void ColorBuffer::SetPixel(const size_t x, const size_t y, const Color &color) const {
-    assert(x < width_ && y < height_);
+    assert(x < width_ && y < height_ && buffer_ != nullptr);
     std::copy_n(color.bgra.begin(), bpp_, buffer_.get() + (x + y * width_) * bpp_);
 }
 
 Color ColorBuffer::GetPixel(const size_t x, const size_t y) const {
-    assert(x < width_ && y < height_);
+    assert(x < width_ && y < height_ && buffer_ != nullptr);
     Color ret = {0, 0, 0, 0};
     std::copy_n(buffer_.get() + (x + y * width_) * bpp_, bpp_, ret.bgra.begin());
     return ret;
+}
+
+void ColorBuffer::Clear(const uint8_t value) const {
+    std::fill_n(buffer_.get(), width_ * height_ * bpp_, value);
+}
+
+DepthBuffer::DepthBuffer()
+    : width_(0), height_(0), buffer_(nullptr) {
+}
+
+DepthBuffer::DepthBuffer(const size_t width, const size_t height)
+    : width_(width), height_(height), buffer_(nullptr) {
+    buffer_ = std::make_unique<float[]>(width * height);
+}
+
+float& DepthBuffer::operator[](const size_t index) {
+    assert(index < width_ * height_);
+    return buffer_[index];
+}
+
+float DepthBuffer::operator[](const size_t index) const {
+    assert(index < width_ * height_);
+    return buffer_[index];
+}
+
+void DepthBuffer::SetDepth(const size_t x, const size_t y, const float depth) const {
+    assert(x < width_ && y < height_ && buffer_ != nullptr);
+    buffer_[x + y * width_] = depth;
+}
+
+float DepthBuffer::GetDepth(const size_t x, const size_t y) const {
+    assert(x < width_ && y < height_ && buffer_ != nullptr);
+    return buffer_[x + y * width_];
+}
+
+void DepthBuffer::Clear(const float value) const {
+    std::fill_n(buffer_.get(), width_ * height_, value);
 }
--- core/buffer.h
+++ core/buffer.h
@@ -25,10 +25,10 @@ public:
     std::uint8_t& operator[](size_t index);
     std::uint8_t  operator[](size_t index) const;
 
-    void Clear(uint8_t value = 0) const;
-
     void SetPixel(size_t x, size_t y, const Color &color) const;
     [[nodiscard]] Color GetPixel(size_t x, size_t y) const;
+
+    void Clear(uint8_t value = 0) const;
 
     [[nodiscard]] size_t width() const { return width_; }
     [[nodiscard]] size_t height() const { return height_; }
@@ -42,6 +42,34 @@ private:
     size_t height_;
     std::uint8_t bpp_;
     std::unique_ptr<std::uint8_t[]> buffer_;
+};
+
+/**
+ * @brief buffer for depth test
+ */
+class DepthBuffer {
+public:
+    DepthBuffer();
+    DepthBuffer(size_t width, size_t height);
+
+    float& operator[](size_t index);
+    float  operator[](size_t index) const;
+
+    void SetDepth(size_t x, size_t y, float depth) const;
+    [[nodiscard]] float GetDepth(size_t x, size_t y) const;
+
+    void Clear(float value = 1.0f) const;
+
+    [[nodiscard]] size_t width() const { return width_; }
+    [[nodiscard]] size_t height() const { return height_; }
+    [[nodiscard]] size_t size() const { return width_ * height_; }
+    [[nodiscard]] const float* buffer() const { return buffer_.get(); }
+    [[nodiscard]]       float* buffer()       { return buffer_.get(); }
+
+private:
+    size_t width_;
+    size_t height_;
+    std::unique_ptr<float[]> buffer_;
 };
 
 #endif //IMAGE_BUFFER_H
