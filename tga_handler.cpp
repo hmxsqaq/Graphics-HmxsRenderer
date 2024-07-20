a9db9cbe2b8120620c338af73684c2e39fb491bb
hmxs <hmxsqaq@163.com>
2024-07-21 03:33:49 +0800

    feat(tga): complete tga_handler

---
+++ tga_handler.cpp
@@ -0,0 +1,203 @@
+#include "tga_handler.h"
+#include <fstream>
+
+std::unique_ptr<ColorBuffer> TGAHandler::read_tga_file(const std::string &filename) {
+    // open file
+    std::ifstream in;
+    in.open(filename, std::ios::binary);
+    if (!in.is_open()) {
+        std::cerr << "read - cannot open file: " << filename << "\n";
+        return nullptr;
+    }
+
+    // read header
+    TGAHeader header;
+    in.read(reinterpret_cast<char *>(&header), sizeof(header));
+    if (!in.good()) {
+        std::cerr << "read - cannot read the TGA header\n";
+        return nullptr;
+    }
+
+    // load width & height & bpp from header
+    const auto width = header.width;
+    const auto height = header.height;
+    const auto bpp = header.bits_per_pixel >> 3; // /= 8
+    if (width <= 0 || height <= 0 || (bpp != GRAYSCALE && bpp != RGB && bpp != RGBA)) {
+        std::cerr << "read - dad width/height/bpp value: width " << width << " height " << height << " bpp " << bpp << "\n";
+        return nullptr;
+    }
+
+    auto buffer = std::make_unique<ColorBuffer>(width, height, bpp);
+
+    // read frame_data
+    if (header.data_type_code == 3 || header.data_type_code == 2) {
+        in.read(reinterpret_cast<char *>(buffer->data()), static_cast<long long>(width) * height * bpp);
+        if (!in.good()) {
+            std::cerr << "read - cannot read frame data\n";
+            return nullptr;
+        }
+    } else if (header.data_type_code == 10 || header.data_type_code == 11) {
+        if (!load_rle_data(in, *buffer)) {
+            std::cerr << "read - cannot load RLE data\n";
+            return nullptr;
+        }
+    } else {
+        std::cerr << "read - unknown file format " << static_cast<int>(header.data_type_code) << "\n";
+        return nullptr;
+    }
+
+    // flip or not
+    if (!(header.image_descriptor & 0x20))
+        buffer->FilpVertically();
+    if (header.image_descriptor & 0x10)
+        buffer->FilpHorizontally();
+    return buffer;
+}
+
+bool TGAHandler::write_tga_file(const std::string &filename, const int width, const int height, const std::uint8_t bpp, const std::uint8_t *data, const bool v_flip, const bool rle) {
+    constexpr std::uint8_t developer_area_ref[4] = {0, 0, 0, 0};
+    constexpr std::uint8_t extension_area_ref[4] = {0, 0, 0, 0};
+    constexpr std::uint8_t footer[18] = {'T','R','U','E','V','I','S','I','O','N','-','X','F','I','L','E','.','\0'};
+
+    // open file
+    std::ofstream out;
+    out.open(filename, std::ios::binary);
+    if (!out.is_open()) {
+        std::cerr << "write - cannot open file: " << filename << "\n";
+        return false;
+    }
+
+    std::cout << "writing - width " << width << " height " << height << " bpp " << static_cast<int>(bpp) << "\n";
+
+    // prepare header
+    TGAHeader header = {};
+    header.bits_per_pixel = bpp << 3; // = * 8
+    header.width  = width;
+    header.height = height;
+    header.data_type_code = bpp == ColorType::GRAYSCALE ? (rle ? 11 : 3) : rle ? 10 : 2;
+    header.image_descriptor = v_flip ? 0x00 : 0x20; // top-left or bottom-left origin
+
+    // write header
+    out.write(reinterpret_cast<const char *>(&header), sizeof(header));
+    if (!out.good()) {
+        std::cerr << "write - cannot dump the tga file\n";
+        return false;
+    }
+
+    // write img
+    if (!rle) {
+        out.write(reinterpret_cast<const char *>(data), width * height * bpp);
+        if (!out.good()) {
+            std::cerr << "write - cannot unload raw frame_data\n";
+            return false;
+        }
+    } else if (!unload_rle_data(out, width, height, bpp, data)) {
+        std::cerr << "write - cannot unload rle frame_data\n";
+        return false;
+    }
+    out.write(reinterpret_cast<const char *>(developer_area_ref), sizeof(developer_area_ref));
+    if (!out.good()) {
+        std::cerr << "write - cannot dump the tga file\n";
+        return false;
+    }
+    out.write(reinterpret_cast<const char *>(extension_area_ref), sizeof(extension_area_ref));
+    if (!out.good()) {
+        std::cerr << "write - cannot dump the tga file\n";
+        return false;
+    }
+
+    // write footer
+    out.write(reinterpret_cast<const char *>(footer), sizeof(footer));
+    if (!out.good()) {
+        std::cerr << "write - cannot dump the tga file\n";
+        return false;
+    }
+
+    std::cout << filename << " has been written successfully\n";
+    return true;
+}
+
+bool TGAHandler::load_rle_data(std::ifstream &in, ColorBuffer &buffer) {
+    const size_t width = buffer.width(), height = buffer.height();
+    const std::uint8_t bpp = buffer.bpp();
+    const size_t pixels_count = width * height;
+    size_t current_pixel = 0;
+    size_t current_byte  = 0;
+    Color color_buffer;
+    do {
+        std::uint8_t chunk_header = in.get();
+        if (!in.good()) {
+            std::cerr << "rle - an error occurred while reading the frame_data\n";
+            return false;
+        }
+        if (chunk_header < 128) {
+            chunk_header++;
+            for (int i = 0; i < chunk_header; i++) {
+                in.read(reinterpret_cast<char *>(color_buffer.bgra.data()), bpp);
+                if (!in.good()) {
+                    std::cerr << "rle - an error occurred while reading the header\n";
+                    return false;
+                }
+                for (int t = 0; t < bpp; t++) buffer[current_byte++] = color_buffer.bgra[t];
+                current_pixel++;
+                if (current_pixel > pixels_count) {
+                    std::cerr << "rle - too many pixels were read\n";
+                    return false;
+                }
+            }
+        } else {
+            chunk_header -= 127;
+            in.read(reinterpret_cast<char *>(color_buffer.bgra.data()), bpp);
+            if (!in.good()) {
+                std::cerr << "rle - an error occurred while reading the header\n";
+                return false;
+            }
+            for (int i=0; i < chunk_header; i++) {
+                for (int t = 0; t < bpp; t++) buffer[current_byte++] = color_buffer.bgra[t];
+                current_pixel++;
+                if (current_pixel > pixels_count) {
+                    std::cerr << "rle - too many pixels were read\n";
+                    return false;
+                }
+            }
+        }
+    } while (current_pixel < pixels_count);
+    return true;
+}
+
+bool TGAHandler::unload_rle_data(std::ofstream &out, const int width, const int height, const std::uint8_t bpp, const std::uint8_t *data) {
+    const size_t pixels_count = width * height;
+    size_t current_pixel = 0;
+    while (current_pixel < pixels_count) {
+        const size_t chunk_start = current_pixel * bpp;
+        size_t current_byte = current_pixel * bpp;
+        std::uint8_t run_length = 1;
+        bool raw = true;
+        while (current_pixel + run_length < pixels_count && run_length < RLE_MAX_CHUNK_LENGTH) {
+            bool is_equal = true;
+            for (int t = 0; is_equal && t < bpp; t++) is_equal = data[current_byte + t] == data[current_byte + t + bpp];
+            current_byte += bpp;
+            if (run_length == 1)
+                raw = !is_equal;
+            if (raw && is_equal) {
+                run_length--;
+                break;
+            }
+            if (!raw && !is_equal)
+                break;
+            run_length++;
+        }
+        current_pixel += run_length;
+        out.put(static_cast<char>(raw ? run_length - 1 : run_length + 127));
+        if (!out.good()) {
+            std::cerr << "rle - can't dump the tga file\n";
+            return false;
+        }
+        out.write(reinterpret_cast<const char *>(data + chunk_start), raw ? run_length * bpp : bpp);
+        if (!out.good()) {
+            std::cerr << "rle - can't dump the tga file\n";
+            return false;
+        }
+    }
+    return true;
+}
---
+++ tga_handler.h
@@ -0,0 +1,45 @@
+#ifndef TGA_HANDLER_H
+#define TGA_HANDLER_H
+
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include "buffer.h"
+
+#define RLE_MAX_CHUNK_LENGTH 128
+
+#pragma pack(push, 1)
+/**
+ * @brief Standard TGA header
+ */
+struct TGAHeader {
+    std::uint8_t  id_length = 0;
+    std::uint8_t  color_map_type = 0;
+    std::uint8_t  data_type_code = 0;
+    std::uint16_t color_map_origin = 0;
+    std::uint16_t color_map_length = 0;
+    std::uint8_t  color_map_depth = 0;
+    std::uint16_t x_origin = 0;
+    std::uint16_t y_origin = 0;
+    std::uint16_t width = 0;
+    std::uint16_t height = 0;
+    std::uint8_t  bits_per_pixel = 0;
+    std::uint8_t  image_descriptor = 0;
+};
+#pragma pack(pop)
+
+/**
+ * @brief .tga file handler, including rle encoding/decoding
+ */
+class TGAHandler {
+public:
+    TGAHandler() = delete;
+
+    static std::unique_ptr<ColorBuffer> read_tga_file(const std::string &filename);
+    static bool write_tga_file(const std::string &filename, int width, int height, std::uint8_t bpp, const std::uint8_t *data, bool v_flip = false, bool rle = true);
+private:
+    static bool load_rle_data(std::ifstream &in, ColorBuffer &buffer);
+    static bool unload_rle_data(std::ofstream &out, int width, int height, std::uint8_t bpp, const std::uint8_t *data);
+};
+
+#endif //TGA_HANDLER_H
